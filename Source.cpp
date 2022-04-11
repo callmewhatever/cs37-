d619949168cf1a34204bad52f91f2cde4e55d989
callmewhatever <91353182+callmewhatever@users.noreply.github.com>
2022-04-10 17:09:53 -0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -85,3 +85,25 @@ int main()
 
 	return 0;
 }
+/*
+Two complex numbers have created, the two complex numbers are:
+3.62+1.25i.
+0.32+4.15i.
+
+The sum of two complex numbers is:
+3.94+5.4i.
+
+The difference of two complex numbers is:
+3.3-2.9i.
+
+The product of two complex numbers is:
+-4.0291+15.423i.
+
+Those two complex numbers after being cubed are:
+30.4692+47.1884i.
+-16.5008-70.1985i.
+
+C:\Users\rafon\source\repos\c++ p3\x64\Debug\c++ p3.exe (process 3060) exited with code 0.
+To automatically close the console when debugging stops, enable Tools->Options->Debugging->Automatically close the console when debugging stops.
+Press any key to close this window . . .
+*/
